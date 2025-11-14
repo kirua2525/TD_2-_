4e0612f8619c5bf7d8600eb411c660c597a8cdcf
悠陽 宍戸 <k025g0067@g.neec.ac.jp>
2025-11-14 11:10:54 +0900

    プレイヤーの横移動とジャンプと描画 (version.1)

---
+++ player.cpp
@@ -0,0 +1,66 @@
+#include <Novice.h>
+#include <math.h>
+#include "Vector2.h"
+#include "player.h"
+
+void Player::Initialize(
+	Vector2 playerPos,
+	Vector2 playerVelocity,
+	Vector2 playerSize,
+	Vector2 playerRadius,
+	float playerAngle,
+	unsigned int playerColor,
+	//int textureHandle
+	float playerJumpSpeed,
+	bool playerAlive,
+	bool playerVisible,
+	bool playerJumping,
+	bool playerMoving,
+	bool playerOnGround
+) {
+	this->pos = playerPos;
+	this->velocity = playerVelocity;
+	this->size = playerSize;
+	this->radius = playerRadius;
+	this->angle = playerAngle;
+	this->color = playerColor;
+	//this->texture = textureHandle;
+	this->jumpSpeed = playerJumpSpeed;
+	this->isAlive = playerAlive;
+	this->isVisible = playerVisible;
+	this->isjumping = playerJumping;
+	this->isMoving = playerMoving;
+	this->isOnGround = playerOnGround;
+}
+
+void Player::Update(const char* keys) {
+	if(keys[DIK_A]||keys[DIK_LEFT]) {
+		this->pos.x -= 5.0f;
+	}
+	if(keys[DIK_D]||keys[DIK_RIGHT]) {
+		this->pos.x += 5.0f;
+	}
+	if((keys[DIK_W]||keys[DIK_UP]||keys[DIK_SPACE]) && this->isOnGround) {
+		this->velocity.y = -this->jumpSpeed;
+		this->isOnGround = false;
+	}
+	this->velocity.y += 0.5f; // Gravity
+	this->pos.y += this->velocity.y;
+	if(this->pos.y >= 600.0f) { // Ground collision
+		this->pos.y = 600.0f;
+		this->velocity.y = 0.0f;
+		this->isOnGround = true;
+	}
+}
+
+void Player::Draw() {
+	Novice::DrawBox(
+		(int)this->pos.x,
+		(int)this->pos.y,
+		(int)this->radius.x,
+		(int)this->radius.y,
+		this->angle,
+		this->color,
+		kFillModeSolid
+	);
+}
---
+++ player.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Vector2.h"
+
+class Player {
+public:
+	Vector2 pos;
+	Vector2 velocity;
+	Vector2 size;
+	Vector2 radius;
+	float angle;
+	unsigned int color;
+	//int texture;
+	float jumpSpeed;
+	bool isAlive; 
+	bool isVisible; 
+	bool isjumping;
+	bool isMoving; 
+	bool isOnGround;
+
+public:
+	void Initialize(
+		Vector2 pos,
+		Vector2 velocity,
+		Vector2 size,
+		Vector2 playerRadius,
+		float angle,
+		unsigned int enemyColor,
+		//int textureHandle
+		float jumpSpeed,
+		bool alive,	
+		bool visible,
+		bool jumping,
+		bool moving,
+		bool onGround
+	);
+
+	void Update(const char* keys);
+
+	void Draw();
+};
